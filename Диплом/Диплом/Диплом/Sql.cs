205b70a59707ccfbf8d304edef4a976cfe17c3a5
MirnII <49513103+MirnII@users.noreply.github.com>
2019-04-11 23:46:04 +0900

    Add files via upload

---
+++ Диплом/Диплом/Диплом/Sql.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Analytics.Interfaces;
+using Microsoft.Analytics.Types.Sql;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace Диплом
+{
+    class Sql
+    {
+        string Sql_ = ConfigurationManager.ConnectionStrings["SQL"].ConnectionString;
+    }
+}
